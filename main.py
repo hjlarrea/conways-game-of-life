9ccd0b1fd1c500fff96c2b4d55bb63a9d3acab66
Hernan J. Larrea <hernan@larrea.com.ar>
2024-03-31 23:30:08 -0300

    Added Board class, refactoring the program code.

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def draw():
         screen.draw.rect(Rect((button["button"]["x"],button["button"]["y"]),(button["button"]["width"],button["button"]["height"])),(255,255,255))
 
     # Render Status Messages
-    if gameMode == 0:
+    if board.gameMode == 0:
         screen.draw.text("Status: Paused", midleft=(buttonLeftMargin,HEIGHT - 110))
-    elif gameMode == 1 or gameMode == 3:
+    elif board.gameMode == 1 or board.gameMode == 3:
         screen.draw.text("Status: In Progress", midleft=(buttonLeftMargin,HEIGHT - 110))
-    screen.draw.text(f"Generation: {generation}", midleft=(buttonLeftMargin,HEIGHT - 70))
-    screen.draw.text(f"Alive Cells: {aliveCells}", midleft=(buttonLeftMargin,HEIGHT - 30))
+    screen.draw.text(f"Generation: {board.generation}", midleft=(buttonLeftMargin,HEIGHT - 70))
+    screen.draw.text(f"Alive Cells: {board.aliveCells}", midleft=(buttonLeftMargin,HEIGHT - 30))
 
     # Render Main Board Square
     screen.draw.rect(Rect((margingSpacing,margingSpacing),(boardSizePx,boardSizePx)),(255,255,255))
@@ -26,49 +26,48 @@ def draw():
     # Draw generation on the screen
     for i in range(0,boardSize):
         for j in range(0,boardSize):
-            if board[i][j].alive:
-                screen.draw.filled_rect(Rect(board[i][j].x,board[i][j].y,board[i][j].xWidth,board[i][j].yWidth),(255,255,255))
+            if board.state[i][j].alive:
+                screen.draw.filled_rect(Rect(board.state[i][j].x,board.state[i][j].y,board.state[i][j].xWidth,board.state[i][j].yWidth),(255,255,255))
             else:
-                if gameMode != 1:
-                    screen.draw.line((board[i][j].x,board[i][j].y+9),(board[i][j].x+9,board[i][j].y+9),(255,255,255))
-                    screen.draw.line((board[i][j].x+9,board[i][j].y),(board[i][j].x+9,board[i][j].y+9),(255,255,255))
+                if board.gameMode != 1:
+                    screen.draw.line((board.state[i][j].x,board.state[i][j].y+9),(board.state[i][j].x+9,board.state[i][j].y+9),(255,255,255))
+                    screen.draw.line((board.state[i][j].x+9,board.state[i][j].y),(board.state[i][j].x+9,board.state[i][j].y+9),(255,255,255))
 
 # Event hooks
 
 def on_mouse_down(pos):
-    global state
+    global board
     global dragging
-    global gameMode
 
     clicked = clickedButton(pos)
     if clicked == 0:
-        gameMode = 1
+        board.gameMode = 1
     elif clicked == 1:
-        gameMode = 0
-        resetBoard()
+        board.gameMode = 0
+        board.resetBoard()
     elif clicked == 2:
         gameMode = 0
     elif clicked == 3:
-        gameMode = 3
-    elif gameMode != 1: # Block making further changes one the game has started
+        board.gameMode = 3
+    elif board.gameMode != 1: # Block making further changes one the game has started
         row,col = returnRowCol(pos)
         if posInBoard(pos):
             dragging = True
-            if (board[row][col]).isAlive(): #Activate or deactivate on click depending on previous state
-                board[row][col].setDead()
+            if (board.state[row][col]).isAlive(): #Activate or deactivate on click depending on previous state
+                board.state[row][col].setDead()
             else:
-                board[row][col].setAlive()
+                board.state[row][col].setAlive()
 
 def on_mouse_up():
     global dragging
     dragging = False
 
 def on_mouse_move(pos):
-    global state
-    if gameMode != 1: # Block making further changes one the game has started
+    global board
+    if board.gameMode != 1: # Block making further changes one the game has started
         row,col = returnRowCol(pos)
         if posInBoard(pos) and dragging == True:
-            board[row][col].setAlive()
+            board.state[row][col].setAlive()
 
 # Helper functions
 
@@ -94,80 +93,63 @@ def returnRowCol(pos):
     col = int(x/chunkSize) - 1
     return row,col
 
-# Sets board to next generation based on the previous one
-def nextGen():
-    global board
-    global gameMode
-    global generation
-    global aliveCells
-    if gameMode == 1 or gameMode == 3:
-        board2 = [[Cell(x=i*10+10,y=j*10+10,row=i,col=j) for i in range(0,boardSize)] for j in range(0,boardSize)]
-        for i in range(0,boardSize):
-            for j in range(0,boardSize):
-                if board[j][i].aliveNeightbours() < 2:
-                    board2[j][i].setDead()
-                elif board[j][i].aliveNeightbours() > 3:
-                    board2[j][i].setDead()
-                elif board[j][i].aliveNeightbours() == 3:
-                    board2[j][i].setAlive()
-                else:
-                    board2[j][i] = board[j][i]
-        if gameMode == 3:
-            gameMode = 0
-        board = board2
-        generation += 1
-        aliveCells = len([board[i][j] for i in range(0,boardSize) for j in range(0,boardSize) if board[i][j].isAlive()])
-
-def resetBoard():
-    global board
-    global generation
-    global aliveCells
-    generation = 0
-    aliveCells = 0
-    board = [[Cell(x=i*10+10,y=j*10+10,row=i,col=j) for i in range(0,boardSize)] for j in range(0,boardSize)]
-
+# Main function
 def main():
-    resetBoard()
-    clock.schedule_interval(nextGen, 1.0) # Schedule board updates
+    board.resetBoard()
+    clock.schedule_interval(board.nextGen, 1.0) # Schedule board updates
     pgzrun.go()
 
 # Board class
     
 class Board():
 
+    generation = 0
+    aliveCells = 0
+    gameMode = 0
+
     def __init__(self):
-        pass
+        self.state =  [[Cell(x=i*10+10,y=j*10+10,row=i,col=j) for i in range(0,boardSize)] for j in range(0,boardSize)]
 
-    def resetBoard():
-        global board
-        global generation
-        global aliveCells
-        generation = 0
-        aliveCells = 0
-        board = [[Cell(x=i*10+10,y=j*10+10,row=i,col=j) for i in range(0,boardSize)] for j in range(0,boardSize)]
+    def getCell(self,row,col):
+        return self.state[row][col].isAlive
+    
+    def setCellDead(self,row,col):
+        self.state[row][col].setDead()
+    
+    def setCellAlive(self,row,col):
+        self.state[row][col].setAlive()
 
-    def nextGen():
-        global board
-        global gameMode
-        global generation
-        global aliveCells
-        if gameMode == 1 or gameMode == 3:
-            board2 = [[Cell(x=i*10+10,y=j*10+10,row=i,col=j) for i in range(0,boardSize)] for j in range(0,boardSize)]
+    def aliveNeightbours(self,row,col):
+        aliveNeightbours = 0
+        neightbours = self.state[row][col].boardNeightbours()
+        for row,col in neightbours:
+            if self.state[row][col].alive:
+                aliveNeightbours+=1
+        return aliveNeightbours
+
+    def resetBoard(self):
+        self.state = [[Cell(x=i*10+10,y=j*10+10,row=i,col=j) for i in range(0,boardSize)] for j in range(0,boardSize)]
+        self.generation = 0
+        self.aliveCells = 0
+        
+    def nextGen(self):
+        if board.gameMode == 1 or board.gameMode == 3:
+            state2 = [[Cell(x=i*10+10,y=j*10+10,row=i,col=j) for i in range(0,boardSize)] for j in range(0,boardSize)]
             for i in range(0,boardSize):
                 for j in range(0,boardSize):
-                    if board[j][i].aliveNeightbours() < 2:
-                        board2[j][i].setDead()
-                    elif board[j][i].aliveNeightbours() > 3:
-                        board2[j][i].setDead()
-                    elif board[j][i].aliveNeightbours() == 3:
-                        board2[j][i].setAlive()
+                    if self.aliveNeightbours(row=j,col=i) < 2:
+                        state2[j][i].setDead()
+                    elif self.aliveNeightbours(row=j,col=i) > 3:
+                        state2[j][i].setDead()
+                    elif self.aliveNeightbours(row=j,col=i) == 3:
+                        state2[j][i].setAlive()
                     else:
-                        board2[j][i] = board[j][i]
-            if gameMode == 3:
-                gameMode = 0
-            board = board2
-            generation += 1
-            aliveCells = len([board[i][j] for i in range(0,boardSize) for j in range(0,boardSize) if board[i][j].isAlive()])
+                        state2[j][i] = self.state[j][i]
+            if self.gameMode == 3:
+                self.gameMode = 0
+            self.state = state2
+            self.generation += 1
+            self.aliveCells = len([self.state[i][j] for i in range(0,boardSize) for j in range(0,boardSize) if self.state[i][j].isAlive()])
 
 # Cell class
 
@@ -198,14 +180,6 @@ class Cell():
                 if (i != self.row or j != self.col) and 0 <= i < boardSize and 0 <= j < boardSize:
                     neighbors.append((j, i))
         return neighbors
-    
-    def aliveNeightbours(self):
-        aliveNeightbours = 0
-        neightbours = self.boardNeightbours()
-        for row,col in neightbours:
-            if board[row][col].alive:
-                aliveNeightbours+=1
-        return aliveNeightbours
 
 # Variables: Configuration
 boardSize = 80 # Number of cells (e.g. 80 = 80*80)
@@ -246,11 +220,8 @@ buttons = [ # Screen elements to be rendered
 ]
 
 # Variables: Flow Control
-gameMode = 0
-generation = 0
-aliveCells = 0
 dragging = False
-board = ""
 
 # Start
+board = Board()
 main()
